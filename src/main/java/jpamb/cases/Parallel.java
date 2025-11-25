7b40c215d1c87b330789a20285718d227945d9f3
Oliver Hansen <s224754@dtu.dk>
2025-11-25 12:34:09 +0100

    Add manyBranches case and update .gitignore for IDE files

--- src/main/java/jpamb/cases/Parallel.java
+++ src/main/java/jpamb/cases/Parallel.java
@@ -3,23 +3,24 @@ package jpamb.cases;
 import jpamb.utils.Case;
 
 public class Parallel {
-  @Case("(1) -> ok")
-  public static void manyBranches(int n) {
-      if (n > 100) {
-          while (n < 1000) {
-              n++;
-          }
-    else if (n > 90) {
-          while (n < 900) {
-              n++;
-          }
-    } else if (n > 80) {
-          while (n < 800) {
-              n++;
-          }
-    } else {
-        assert n > 0;
+    @Case("(1) -> ok")
+    public static void manyBranches(int n) {
+        if (n > 100) {
+            while (n < 1000) {
+                n++;
+            }
+        } else if (n > 90) {
+            while (n < 900) {
+                n++;
+            }
+        } else if (n > 80) {
+            while (n < 800) {
+                n++;
+            }
+        } else {
+            assert n > 0;
+        }
     }
-  }
 }
+
 
